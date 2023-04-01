c2f7e8502ed3c74100cfe9b553b6095f77a4f6fb
Ishan Bhat <grbmax@gmail.com>
2023-04-01 13:16:54 +0530

    100 followers

--- app.js
+++ app.js
@@ -4,20 +4,26 @@ const app = express();
 
 app.get('/overlapping-followers', async (req, res) => {
   const ig = new IgApiClient();
-  const user = 'dele388';
-  const pwd = 'rtx@2060super';
+  const user = 'user';
+  const pwd = 'pwd';
+
   const a1 = 'zgodofficial';
   const a2 = 'hastar_og';
 
   await ig.state.generateDevice(user);
   await ig.account.login(user, pwd);
+
   const { pk: account1Id } = await ig.user.searchExact(a1);
   const { pk: account2Id } = await ig.user.searchExact(a2);
+  
   const account1Followers = await ig.feed.accountFollowers(account1Id).items();
   const account2Followers = await ig.feed.accountFollowers(account2Id).items();
+  
   const overlappingFollowers = account1Followers.filter(
-    (follower1) => account2Followers.some((follower2) => follower2.pk === follower1.pk)
-  );
+    (follower1) => account2Followers.some((follower2) => follower2.pk === follower1.pk));
+
+
+
   res.json({ overlappingFollowersCount: overlappingFollowers.length });
 });
 
