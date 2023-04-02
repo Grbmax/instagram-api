d4d8f9166d5a5cf102103a2b9f26aa6088546b3c
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2023-04-02 12:57:34 +0530

    100 followers

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 const express = require('express');
 const { IgApiClient } = require('instagram-private-api');
+
 const app = express();
 
-app.get('/overlapping-followers', async (req, res) => {
-  const ig = new IgApiClient();
-  const user = 'user';
-  const pwd = 'pwd';
+app.get('/followers', async (req, res) => {
+  try {
+    const ig = new IgApiClient();
+    const user = 'user';
+    const pwd = 'pwd';
 
-  const a1 = 'zgodofficial';
-  const a2 = 'hastar_og';
+   
+    console.log(user, pwd)
+    await ig.state.generateDevice(user);
+    await ig.account.login(user, pwd);
+    
 
-  await ig.state.generateDevice(user);
-  await ig.account.login(user, pwd);
-
-  const { pk: account1Id } = await ig.user.searchExact(a1);
-  const { pk: account2Id } = await ig.user.searchExact(a2);
+    const { pk: account1Id } = await ig.user.searchExact(a1);
+    const { pk: account2Id } = await ig.user.searchExact(a2);
+    
+    const account1Followers = await ig.feed.accountFollowers(account1Id).items();
+    const account2Followers = await ig.feed.accountFollowers(account2Id).items();
+    
+    const overlappingFollowers = account1Followers.filter(
+      (follower1) => account2Followers.some((follower2) => follower2.pk === follower1.pk));
   
-  const account1Followers = await ig.feed.accountFollowers(account1Id).items();
-  const account2Followers = await ig.feed.accountFollowers(account2Id).items();
   
-  const overlappingFollowers = account1Followers.filter(
-    (follower1) => account2Followers.some((follower2) => follower2.pk === follower1.pk));
+  
+    res.json({ 
+      account1 : account1Followers,
+      account2 : account2Followers,
+      overlappingFollowersCount: overlappingFollowers.length 
+    });
 
-
-
-  res.json({ overlappingFollowersCount: overlappingFollowers.length });
+  } catch (err) {
+    console.log({ err });
+  }
 });
 
 app.listen(3000, () => console.log('Server started on port 3000'));
